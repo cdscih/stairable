8132dfc242596c04cecc11941d9c8aff39876032
Claudio Rotolo <claudio.work69@gmail.com>
2022-05-28 21:02:02 +0000

    add: incremental test algorithm

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,63 @@
 import autocannon from 'autocannon';
 
 interface Options {
-  urls: string[],
-  rps: number,
-//   inputNumLimit: 10,
-//   responseTimeLimitMs: 30,
+  url: string,
+  expectedAvgMsResponseTime: number,
+  expectedMaxStdDevMsResponseTime: number,
+  method?: autocannon.Request["method"],
+  duration?: number,
+  threads?: number,
+  //   inputNumLimit: 10,
 }
 
-export async function fire({ urls, rps }: Options) {
-  return urls.map((url) => autocannon({
+export async function isScalableEnough(opts: Options) {
+  const {
     url,
-    amount: rps,
-    timeout: 1,
-  }, console.log));
+    method,
+    expectedAvgMsResponseTime,
+    expectedMaxStdDevMsResponseTime,
+    duration,
+    threads
+  } = opts
+
+  const output: Record<string, any> = { url }
+
+  const connectionsTests = [1, 10, 100, 1000, 10000]
+
+  let connections = 1
+  let level = 0
+  let outcome
+
+  do {
+    const result = await autocannon({
+      url,
+      method: method ?? 'GET',
+      duration: duration ?? 5,
+      workers: threads ?? 1,
+      connections: connections
+    })
+
+    outcome = (result.latency.average < expectedAvgMsResponseTime && result.latency.stddev < expectedMaxStdDevMsResponseTime) ? "success" : "failure"
+
+    output[level] = {
+      outcome,
+      expectedAvgMsResponseTime,
+      expectedMaxStdDevMsResponseTime,
+      avgMs: result.latency.average,
+      stddev: result.latency.stddev,
+      non2xx: result.non2xx,
+      rps: result.requests.sent
+    }
+    connections *= 10
+    level++
+  } while (outcome == "success");
+
+  return output
 }
 
-fire({
-  urls: ['example.com'],
-  rps: 1000,
-});
+isScalableEnough({
+  url: 'example.com',
+  method: "GET",
+  expectedAvgMsResponseTime: 1,
+  expectedMaxStdDevMsResponseTime: 1
+}).then((res) => console.log(res));
--- test/fire.test.ts
+++ test/fire.test.ts
@@ -1,7 +1,34 @@
-import { fire } from "../lib";
+import { isScalableEnough } from "../lib";
+import express from 'express';
+
+jest.setTimeout(20000)
 
 describe('Fire', () => {
+
+  let server: any
+
+  beforeAll(() => {
+    const app = express()
+    const port = 3000
+
+    app.get('/', (req: any, res: any) => {
+      res.send('test')
+    })
+
+    server = app.listen(port)
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
   test('basic use', async () => {
-    expect(await fire({ urls: ['example.com'], rps: 10 })).toBeDefined();
+    const res = await isScalableEnough({
+      url: 'localhost:3000',
+      method: "GET",
+      expectedAvgMsResponseTime: 1,
+      expectedMaxStdDevMsResponseTime: 1
+    })
+    expect(res[1]).toBeDefined();
   });
 });

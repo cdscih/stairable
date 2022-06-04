903626e1c933f1553f10236ad0e8099d4b672dfe
Claudio Rotolo <54625689+claudio-di-sciacca@users.noreply.github.com>
2022-06-04 22:33:47 +0200

    add: support for body creation function (#10)

---
+++ lib/types.ts
@@ -0,0 +1,50 @@
+import type autocannon from 'autocannon'
+
+export interface FireResult {
+  latency: autocannon.Result['latency']
+  requests: autocannon.Result['requests']
+  non2xx: autocannon.Result['non2xx']
+  maxBodyLenght?: number
+}
+
+export interface ConnectionsTest {
+  success?: boolean
+  connections: number
+  rps: number
+  avgMs: number
+  stddev: number
+  non2xx: number
+}
+
+export interface Requirements {
+  maxResTime: number
+  minRPS: number
+  minConnections?: number
+}
+
+export type CreateBody = (n: number) => string | Buffer | undefined
+
+export interface Options {
+  url: string
+  requirements: Requirements
+  headers?: Record<string, string | string[]>
+  method?: autocannon.Request['method']
+  body?: {
+    create: CreateBody,
+    maxNs: number
+  },
+  inputLimit?: number
+  connectionsMultipler?: number,
+  bodyNMultiplier?: number,
+  connectionTestsDuration?: number
+  workers?: number
+  verbose?: boolean
+}
+
+export interface Result {
+  url: string
+  requirements?: Requirements
+  meetsRequirements: boolean
+  best: ConnectionsTest
+  tests?: ConnectionsTest[]
+}
--- samples/3000.sample.ts
+++ samples/3000.sample.ts
@@ -1,8 +1,18 @@
 import { Stairable } from './../lib'
 
-new Stairable(
-  'localhost:3000',
-  {
-    maxResTime: 100, minRPS: 100000
+const createBody = (n: number): string => {
+  const items = []
+  for (let i = 0; i < n; i++) {
+    items.push(i)
   }
-).launch().then(console.log).catch(() => ({}))
+  return JSON.stringify({ items })
+}
+
+new Stairable(
+  {
+    url: 'localhost:3000',
+    requirements: { maxResTime: 100, minRPS: 50000 },
+    body: { create: createBody, maxNs: 1000 },
+    connectionTestsDuration: 5
+  }
+).launch().then(console.log).catch(console.error)
